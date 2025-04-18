bb53e4bf3ba0eb96bc3844310ff2c6a57057b74c
Denis Dovnar <denis.dovnar02@gmail.com>
2025-04-18 21:29:13 +0300

    Remove ignored files

--- CEOSub/Intermediate/Build/Win64/x64/CEOSubEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/CEOSub/CEOSub/CEOSub/Intermediate/Build/Win64/x64/CEOSubEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
--- CEOSub/Intermediate/Build/Win64/x64/CEOSubEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.40.33811
-#include "C:/CEOSub/CEOSub/CEOSub/Intermediate/Build/Win64/x64/CEOSubEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
